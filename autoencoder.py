abf94a5002d2bc553f6429d6ea706eb684939751
Myxogastria <50288647+Myxogastria@users.noreply.github.com>
2020-10-31 15:27:41 +0900

    add forgotten activation

--- autoencoder.py
+++ autoencoder.py
@@ -21,14 +21,14 @@ one_hot_test = keras.utils.to_categorical(label_test, num_classes=10)
 
 
 n_class = 10
-n_filter = 10
-kernel_size = 5
-n_feature = 3
-epochs = 30
-# n_filter = 3
+# n_filter = 10
 # kernel_size = 5
 # n_feature = 3
-# epochs = 3
+# epochs = 30
+n_filter = 3
+kernel_size = 5
+n_feature = 2
+epochs = 3
 
 shape_before_transconv = np.append(np.array(img_size)-kernel_size+1, n_filter)
 
@@ -37,12 +37,13 @@ shape_before_transconv = np.append(np.array(img_size)-kernel_size+1, n_filter)
 input_image = lyr.Input(name='input', shape=(28, 28, 1))
 filtered = lyr.Conv2D(n_filter, name='filter', kernel_size=(kernel_size, kernel_size), activation='relu', input_shape=np.array([*img_size, 1]))(input_image)
 filtered_flattened = lyr.Flatten(name='filter_flatten')(filtered)
-feature_unbounded_flattened = lyr.Dense(n_feature*n_class, name='feature_dense')(filtered_flattened)
+feature_unbounded_flattened = lyr.Dense(n_feature*n_class, name='feature_dense', activation='relu')(filtered_flattened)
 feature_unbounded = lyr.Reshape((n_feature, n_class), name='feature_reshape')(feature_unbounded_flattened)
 
 classifier = lyr.Lambda(lambda x: tf.keras.backend.softmax(tf.keras.backend.sum(x, axis=1)), name='classify')(feature_unbounded)
 
-feature_bounded = lyr.Activation("sigmoid", name='limit')(feature_unbounded)
+feature_bounded = lyr.Lambda(lambda x: x/(x+1), name='limit')(feature_unbounded)
+# feature_bounded = lyr.Activation("sigmoid", name='limit')(feature_unbounded)
 
 input_feature = lyr.Input(name='input_feature', shape=(n_feature, n_class))
 feature_bounded_flattened = lyr.Flatten(name='limit_flatten')(input_feature)
@@ -70,19 +71,23 @@ history = autoencoder.fit(img_train, [one_hot_train, img_train], epochs=epochs)
 pd.DataFrame(history.history).plot()
 plt.show(block=False)
 
+# ちょっと余裕を見たclimにする
+clim = (-0.2, 1.2)
+
 # テスト画像の分類と再現
 plt.close('all')
 for i in range(n_class):
     plt.figure()
-    plt.imshow(img_test[i, :, :, 0])
-    plt.title(label_test[i])
-    plt.show(block=False)
 
-    plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.imshow(img_test[i, :, :, 0], clim=clim)
+    plt.title(label_test[i])
+
+    plt.subplot(1, 2, 2)
     [a, b] = autoencoder.predict(img_test[i:(i+1), :, :, :])
-    plt.imshow(b[0, :, :, 0])
+    plt.imshow(b[0, :, :, 0], clim=clim)
     plt.title(np.argmax(a))
-    plt.show(block=False)
+plt.show(block=False)
 
 # 特徴量行列の特定の成分のみ1にした場合の生成画像
 plt.close('all')
@@ -94,10 +99,10 @@ for i_feature in range(n_feature):
         feature_matrix[0, i_feature, i_class] = 1
 
         plt.subplot(n_feature, n_class, i_plot)
-        plt.imshow(generator.predict(feature_matrix)[0, :, :, 0])
+        plt.imshow(generator.predict(feature_matrix)[0, :, :, 0], clim=clim)
         plt.title('cls:{}, ftr:{}'.format(i_class, i_feature))
-        plt.show(block=False)
         i_plot += 1
+plt.show(block=False)
 
 # 特徴量行列の特定の成分を1に設定し，
 # さらにそれに対応するクラスの成分は大きく，
@@ -112,10 +117,10 @@ for i_feature in range(n_feature):
         feature_matrix[0, i_feature, i_class] = 1
 
         plt.subplot(n_feature, n_class, i_plot)
-        plt.imshow(generator.predict(feature_matrix)[0, :, :, 0])
+        plt.imshow(generator.predict(feature_matrix)[0, :, :, 0], clim=clim)
         plt.title('cls:{}, ftr:{}'.format(i_class, i_feature))
-        plt.show(block=False)
         i_plot += 1
+plt.show(block=False)
 
 # 特徴量行列の特定のクラスの成分のみ1にした場合の生成画像
 plt.close('all')
@@ -126,10 +131,10 @@ for i_class in range(n_class):
     feature_matrix[0, :, i_class] = 1
 
     plt.subplot(1, n_class, i_plot)
-    plt.imshow(generator.predict(feature_matrix)[0, :, :, 0])
+    plt.imshow(generator.predict(feature_matrix)[0, :, :, 0], clim=clim)
     plt.title('cls:{}, ftr:{}'.format(i_class, 'all'))
-    plt.show(block=False)
     i_plot += 1
+plt.show(block=False)
 
 # 特徴量行列の特定のクラスの成分は1に設定し，
 # それ以外は小さく設定した場合の生成画像
@@ -140,15 +145,15 @@ for i_class in range(n_class):
     feature_matrix = np.random.rand(1, n_feature, n_class)/5
     feature_matrix[0, :, i_class] = 1
 
-
     plt.subplot(1, n_class, i_plot)
-    plt.imshow(generator.predict(feature_matrix)[0, :, :, 0])
+    plt.imshow(generator.predict(feature_matrix)[0, :, :, 0], clim=clim)
     plt.title('cls:{}, ftr:{}'.format(i_class, 'all'))
-    plt.show(block=False)
     i_plot += 1
+plt.show(block=False)
 
 
 # 訓練データのクラスごとの特徴量行列の成分の分布
+# 3以外は特徴量2つで十分に見える
 plt.close('all')
 for i_class in range(n_class):
     i_img_train = img_train[label_train==i_class, :, :, :]
@@ -163,7 +168,7 @@ for i_class in range(n_class):
             plt.ylim((0, 10))
             plt.title('cls:{}, ftr:{}'.format(plot_class, plot_feature))
             i_plot += 1
-    plt.show(block=False)
+plt.show(block=False)
 
 # 訓練データのクラスごとに，特徴量0の散布図をプロット
 plt.close('all')
